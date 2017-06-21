edaad61061a76110e7f8851682d108fa0056a249
Jacob Floyd <cognifloyd@gmail.com>
2017-06-21 05:56:58 -0500

    WIP: Add json parameter to clean up stdout into valid JSON

--- actions/ansible_playbook.py
+++ actions/ansible_playbook.py
@@ -1,6 +1,12 @@
 #!/usr/bin/env python
 
+from __future__ import print_function
+
+import os
+import subprocess
+import re
 import sys
+
 from lib.ansible_base import AnsibleBaseRunner
 
 __all__ = [
@@ -37,6 +43,52 @@ class AnsiblePlaybookRunner(AnsibleBaseRunner):
 
     def __init__(self, *args, **kwargs):
         super(AnsiblePlaybookRunner, self).__init__(*args, **kwargs)
+
+    def handle_json_arg(self):
+        os.environ['ANSIBLE_CALLBAKC_PLUGIN'] = 'json'
+        self.stdout = subprocess.PIPE
+        # TODO: --json is probably not be compatible with other options
+        # like syntax-check, verbose, and possibly others
+
+    def popen_call(self, p):
+        """
+        :param subprocess.Popen p:
+        :return:
+        """
+        if not self.json_output:
+            return super(AnsiblePlaybookRunner, self).popen_call(p)
+
+        # lines that should go to stderr instead of stdout
+        stderr_lines = re.compile(
+            r'('
+
+            # these were identified in this closed PR:
+            #    https://github.com/ansible/ansible/pull/17448/files
+            # see https://github.com/ansible/ansible/issues/17122
+            r"^\tto retry, use: --limit @.*$"
+            r'|'
+            r"^skipping playbook include '.*' due to conditional test failure$"
+            r'|'
+            r"^statically included: .*$"
+
+            # other possibilities:
+            # r'|'
+            # r'^to see the full traceback, use -vvv$'
+            # r'|'
+            # r"^The full traceback was:$"
+            # but then I would somehow have to include the traceback as well...
+
+            r')'
+        )
+
+        while p.poll() is None:
+            line = p.stdout.readline()
+            if re.match(stderr_lines, line):
+                print(line, file=sys.stderr)
+            else:
+                print(line)
+
+        return p.returncode
 
 
 if __name__ == '__main__':
